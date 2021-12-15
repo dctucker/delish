4d8679c05aaeb1382169bf681b96c6445f881865
Casey Tucker <dctucker@hotmail.com>
2021-12-15 00:14:55 -0800

    rearranging grammar rules

--- src/deligrammar.nim
+++ src/deligrammar.nim
@@ -24,61 +24,60 @@ const grammar_source_0 = static"""
   Blank         <- ("\\" \n) / \9 / " "
   VLine         <- \n / Comment / Block / Statement Comment* \n
   Comment       <- '#' @ \n
-  Expr          <- VarDeref / Arg / Array / Object / StrBlock / StrLiteral / Integer / Boolean / Path / JsonBlock / Stream
   Block         <- !'$' (Conditional / WhileLoop / ForLoop / Function) / Subshell
-  Conditional   <- "if"    Blank+ Expr     Blank+                         "{" \s* Code* \s* "}" \s*
-  WhileLoop     <- "while" Blank+ Expr     Blank+                         "{" \s* Code* \s* "}" \s*
-  ForLoop       <- "for"   Blank+ Variable Blank+ "in" Blank+ Expr Blank+ "{" \s* Code* \s* "}" \s*
-  Subshell      <- "sub"   Blank+          Blank+                         "{" \s* Code* \s* "}" \s*
-  Function      <- Identifier Blank* "="   Blank*                         "{" \s* Code* \s* "}" \s*
   Statement     <- OpenStmt / AssignStmt / LocalStmt / CloseStmt / ArgStmt / EnvStmt / IncludeStmt / StreamStmt / RunStmt / FunctionStmt
+  Conditional   <- "if"       Blank+ Expr     Blank+                         "{" \s* Code* \s* "}" \s*
+  WhileLoop     <- "while"    Blank+ Expr     Blank+                         "{" \s* Code* \s* "}" \s*
+  ForLoop       <- "for"      Blank+ Variable Blank+ "in" Blank+ Expr Blank+ "{" \s* Code* \s* "}" \s*
+  Function      <- Identifier Blank* "="      Blank*                         "{" \s* Code* \s* "}" \s*
+  Subshell      <- "sub"      Blank+          Blank+                         "{" \s* Code* \s* "}" \s*
+  OpenStmt      <- &'$' Variable Blank* "=" Blank* "open" (Blank+ RedirOp)? Blank+ Path
   AssignStmt    <- &'$' Variable Blank* ( AssignOp / AppendOp / RemoveOp )  Blank* (ArgExpr / Expr / RunStmt)
+  LocalStmt     <- "local" Blank+ Variable ( Blank* "=" Blank* Expr )?
+  CloseStmt     <- &'$' Variable ".close"
+  ArgStmt       <- "arg" ArgNames (Blank* DefaultOp Blank* ArgDefault)?
+  EnvStmt       <- "env" Blank+ Variable (Blank* DefaultOp Blank* EnvDefault)?
+  IncludeStmt   <- "include" Blank+ StrLiteral
+  StreamStmt    <- ( Variable "." )? Stream Blank+ ExprList
+  RunStmt       <- ( (AsyncFlag / RedirFlag / (AsyncFlag RedirFlag) ) Blank+)? "run" Blank+ Invocation ( Blank* "|" Blank* Invocation )*
+  FunctionStmt  <- Identifier (Blank+ Expr)*
+  ArgDefault    <- Expr
+  EnvDefault    <- Expr
+  ExprList      <- Expr ( Blank* "," Blank* Expr Blank* )*
+  Invocation    <- { \w (\w/"-")* } ( Blank+ (Expr / String) )*
+  ArgExpr       <- Arg (Blank+ / '=') Expr?
+  Expr          <- VarDeref / Arg / Array / Object / StrBlock / StrLiteral / Integer / Boolean / Path / JsonBlock / Stream
+  VarDeref      <- &'$' Variable ( [.] ( StrLiteral / Integer / Variable / Identifier ) )*
+  RedirFlag     <- "redir" (Blank+ (Variable / Path / Stream) Blank* RedirOp Blank* (Variable / Path / Stream))+
+  AsyncFlag     <- "async"
+  ArgNames      <- ( Blank+ Arg )+
+  Arg           <- &'-' (ArgLong / ArgShort)
+  ArgShort      <- '-' { \w+ }
+  ArgLong       <- "--" { (\w ('-' \w)*)+ }
+  Array         <- '[' ( \s* Expr Blank* ','? \s* )* ']'
+  Object        <- '[' ( \s* Expr Blank* ':' Blank* Expr Blank* ','? \s* )+ ']'
+  Identifier    <- !Keyword { (\w/"-")+ }
+  JsonBlock     <- "json" Blank+ StrBlock
+  StrBlock      <- (\"\"\") \n @@ (\"\"\")
+  StrLiteral    <- ('"' @@ '"') / ('\'' @@ '\'')
+  String        <- { \S+ }
+  Integer       <- { \d+ }
+  Path          <- { ("."* "/" \S+ ) / "." }
+  Variable      <- '$' { (\w / '-')+ }
+  Keyword       <- "sub" / "if" / "white" / "arg" / "in" / "out" / "err" / "include" / "true" / "false"
+  Boolean       <- { "true" / "false" }
+  Stream        <- &[ioe] (StreamIn / StreamOut / StreamErr)
+  StreamIn      <- "in"
+  StreamOut     <- "out"
+  StreamErr     <- "err"
   AssignOp      <- "="
   AppendOp      <- "+="
   RemoveOp      <- "-="
-  OpenStmt      <- &'$' Variable Blank* "=" Blank* "open" (Blank+ RedirOp)? Blank+ Path
-  CloseStmt     <- &'$' Variable ".close"
-  FunctionStmt  <- Identifier (Blank+ Expr)*
-  IncludeStmt   <- "include" Blank+ StrLiteral
-  RunStmt       <- (RunFlags Blank+)? "run" Blank+ Invocation ( Blank* "|" Blank* Invocation )*
-  RunFlags      <- AsyncFlag / RedirFlag / (AsyncFlag RedirFlag)
-  AsyncFlag     <- "async"
-  RedirFlag     <- "redir" (Blank+ (Variable / Path / Stream) Blank* RedirOp Blank* (Variable / Path / Stream))+
   RedirOp       <- RedirAppendOp / RedirReadOp / RedirWriteOp / RedirDuplexOp
   RedirAppendOp <- ">>"
   RedirReadOp   <- "<"
   RedirWriteOp  <- ">"
   RedirDuplexOp <- "<>"
-  Invocation    <- { \w (\w/"-")* } ( Blank+ (Expr / String) )*
-  String        <- {\S+}
-  EnvStmt       <- "env" Blank+ Variable (Blank* DefaultOp Blank* EnvDefault)?
-  EnvDefault    <- Expr
-  ArgExpr       <- Arg (Blank+ / '=') Expr?
-  ArgStmt       <- "arg" ArgNames (Blank* DefaultOp Blank* ArgDefault)?
-  ArgNames      <- ( Blank+ Arg )+
-  Arg           <- &'-' (ArgLong / ArgShort)
-  ArgShort      <- '-' { \w+ }
-  ArgLong       <- "--" { (\w ('-' \w)*)+ }
-  ArgDefault    <- Expr
-  LocalStmt     <- "local" Blank+ Variable ( Blank* "=" Blank* Expr )?
-  VarDeref      <- &'$' Variable ( [.] ( StrLiteral / Integer / Variable / Identifier ) )*
-  Array         <- '[' ( \s* Expr Blank* ','? \s* )* ']'
-  Object        <- '[' ( \s* Expr Blank* ':' Blank* Expr Blank* ','? \s* )+ ']'
-  Integer       <- { \d+ }
-  Identifier    <- !Keyword { (\w/"-")+ }
-  Keyword       <- "sub" / "if" / "white" / "arg" / "in" / "out" / "err" / "include" / "true" / "false"
-  StrLiteral    <- ('"' @@ '"') / ('\'' @@ '\'')
-  StrBlock      <- (\"\"\") \n @@ (\"\"\")
-  JsonBlock     <- "json" Blank+ StrBlock
-  Path          <- { ("."* "/" \S+ ) / "." }
-  Boolean       <- { "true" / "false" }
-  StreamStmt    <- ( Variable "." )? Stream Blank+ ExprList
-  ExprList      <- Expr ( Blank* "," Blank* Expr Blank* )*
-  Stream        <- &[ioe] (StreamIn / StreamOut / StreamErr)
-  StreamIn      <- "in"
-  StreamOut     <- "out"
-  StreamErr     <- "err"
-  Variable      <- '$' { (\w / '-')+ }
   DefaultOp     <- "|="
 """.replace('\n', '\0')
 
