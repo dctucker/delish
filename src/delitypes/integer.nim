b482eba9b447003a872d47d934481f310e185e8f
Casey Tucker <dctucker@hotmail.com>
2025-10-03 22:45:39 -0700

    fix Integer.{oct,hex}

--- src/delitypes/integer.nim
+++ src/delitypes/integer.nim
@@ -8,13 +8,13 @@ proc dOct(nodes: varargs[DeliNode]): DeliNode =
   argvars
   nextarg dkInteger
   maxarg
-  return DKStr("0" & arg.intVal.toOct(arg.intVal.sizeof).strip(chars={'0'}))
+  return DKStr("0" & arg.intVal.toOct(arg.intVal.sizeof).strip(chars={'0'}, trailing=false))
 
 proc dHex(nodes: varargs[DeliNode]): DeliNode =
   argvars
   nextarg dkInteger
   maxarg
-  return DKStr("0x" & arg.intVal.toHex.strip(chars={'0'}))
+  return DKStr("0x" & arg.intVal.toHex.strip(chars={'0'}, trailing=false))
 
 let IntegerFunctions*: Table[string, proc(nodes: varargs[DeliNode]): DeliNode {.nimcall.} ] = {
   "oct": dOct,
